6ffeb5afe77851304615c70faeb2758addb1b884
kanekomasanori <kaneko(a)lanches.co.jp>
2012-10-08 10:42:33 +0900

    changed local.zsh

--- .zsh/local.zsh
+++ .zsh/local.zsh
@@ -1,4 +1,7 @@
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+PATH=$PATH:/opt/local/bin # Mac Ports PATH
+
+export CC=/usr/bin/gcc-4.2
 
 # RVM
 [ -s ${HOME}/.rvm/scripts/rvm ] && source ${HOME}/.rvm/scripts/rvm
